7b0a564c6fc58f78f528d59b299f1c508cf26be0
jibesh <jibeshshrestha00@gmail.com>
2026-02-12 12:08:39 +0545

    add user data script to the ec2 instance

--- main.tf
+++ main.tf
@@ -6,6 +6,15 @@ resource "aws_instance" "example" {
   ami = "ami-0b6c6ebed2801a5cb"
   instance_type = "t3.micro"
 
+  user_data = <<-EOF
+              #!/bin/sh
+              echo "Hello World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
+  // the user_data_replace_on_change option is set to true so that when you change the user_data parameter and run apply, terraform will terminate the origin instance and launch a new one
+  user_data_replace_on_change = true
+
   tags = {
     Name = "terraform-example"
   }
